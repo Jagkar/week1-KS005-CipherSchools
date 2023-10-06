c2318b724827eeabc0b40c5e321a5ddfdb7be8d0
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2023-10-07 00:19:03 +0530

    Add files via upload

---
+++ Lecture6II.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int i,j,k;
+    // i=1;
+    // for(i=1;;){
+    //     cout<<i<<" ";
+    // }
+    // cout<<i++<<endl;
+    // cout<<i<<endl;
+    // cout<<++i<<endl;
+    // cout<<i<<endl;
+
+    // j=0,k=0,i=0;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // j=i++;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // k=++i;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+
+    int i;
+    // i=i+1;
+    for ( i = 1; i < 101; i++)//  called the update which happen after the iteration part of loop is completed
+    {
+        // block which get executed everytime
+        cout<<i<<" ";
+    }
+    
+
+    return 0;
+}
