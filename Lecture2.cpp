dbd6e42efbd08b6fda389324f59591ee14390595
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2023-10-06 22:48:40 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // This is a Header 
+using namespace std;//std makes sure that cout is a part of "iostream"
+/*
+comment
+*/
+int main() // we have written main "function"
+{
+    cout<<"Hello wordl"; //this will print out hello world on user screen
+    return 0;
+}
