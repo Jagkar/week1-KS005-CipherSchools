cbe5da665bf332d7a2bfc726df04caefc2aadc16
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2023-10-07 00:04:59 +0530

    Add files via upload

---
+++ Lecture6I.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // cout<<1;
+    // cout<<2<<3<<4;
+    //in looping a similar action is repeated
+    int i;
+    for ( i = 1; i <= 100; i++)
+    {
+        cout<<i<<" ";
+    }
+    
+    return 0;
+}
