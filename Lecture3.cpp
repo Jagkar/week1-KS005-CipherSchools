c93601f40aff143b67b3c3c8cd6e47c681487be9
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2023-10-06 23:07:27 +0530

    Add files via upload

---
+++ Lecture3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // declare 2 variables and then assign them values of 3,5 and then add them
+    int a=3;
+    int b=5;
+    // a=3;
+    // b=5;
+    // int c;
+    // c=a+b;
+    // cout<<c;
+    cout<<a+b<<endl<<"a+b";
+    cout<<a+b;
+    cout<<endl;
+    cout<<"a+b";
+    return 0;
+}
